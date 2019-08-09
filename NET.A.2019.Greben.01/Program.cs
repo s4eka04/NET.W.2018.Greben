7abfe595b66f70645b58776bb438d21720b3b7a7
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-08-09 19:23:35 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.01/Program.cs
@@ -0,0 +1,62 @@
+﻿using MySort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using MySort;
+
+namespace TestSortingMothods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeArray = 20;
+            int maxValue = 100;
+            int[] arrayIntForFastSort = new int[sizeArray];
+            int[] arrayIntForMergeSort = new int[sizeArray];
+            Random rand = new Random();
+            
+            // Fill the array of random  numbers for FastSort
+            for(int i = 0; i <arrayIntForFastSort.Length; i++)
+            {
+                arrayIntForFastSort[i] = rand.Next(maxValue);
+            }
+
+            //Fill the array of random numbers for MergeSort 
+            for (int i = 0; i < arrayIntForMergeSort.Length; i++)
+            {
+                arrayIntForMergeSort[i] = rand.Next(maxValue);
+            }
+
+            // Call FastSort method
+            Console.WriteLine("Before FastSort: ");
+            ConsoleArray(arrayIntForFastSort);
+            Sort.FastSort(arrayIntForFastSort, 0, arrayIntForFastSort.Length - 1);
+            Console.WriteLine("After FastSort: ");
+            ConsoleArray(arrayIntForFastSort);
+
+            Console.WriteLine(new String('*', 70));
+
+
+            //Call MergeSort metod
+            Console.WriteLine("Before MergeSort: ");
+            ConsoleArray(arrayIntForMergeSort);
+            Sort.MergeSort(arrayIntForMergeSort);
+            Console.WriteLine("After MergeSort: ");
+            ConsoleArray(arrayIntForMergeSort);
+            
+        }
+
+        private static void ConsoleArray(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++ )
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.Write('\n');
+        }
+
+    }
+}
---
+++ NET.A.2019.Greben.01/Sort.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySort
+{
+    public static class Sort
+    {
+
+        //**********************FAST SORT****************************
+        public static void FastSort(int[] array, int left, int right)
+        {
+
+            if (left < right)
+            {
+                //Choose a random number 
+                Random random = new Random();
+                int randomNumber = random.Next(left, right);
+
+                //Put the element under the index random in place and divide the array into two parts
+                int pointerSortedItem = SplitIntoTwo(array, left, right, randomNumber);
+
+                //repeat with the left part of array
+                FastSort(array, left, pointerSortedItem - 1);
+                // or the right part
+                FastSort(array, pointerSortedItem + 1, right);
+
+            }
+
+        }
+
+        private static void Swap(int[] arr, int firstPointer, int secondPointer)
+        {
+            int tempInteger = arr[firstPointer];
+            arr[firstPointer] = arr[secondPointer];
+            arr[secondPointer] = tempInteger;
+        }
+
+        private static int SplitIntoTwo(int[] array, int left, int right, int randomNumber)
+        {
+
+            int pointerSortedItem = left;
+            int pointerValue = array[randomNumber];
+
+            Swap(array, randomNumber, right);
+            for (int i = left; i < right; i++)
+            {
+                if (array[i] < pointerValue)
+                {
+                    if (i != pointerSortedItem)
+                        Swap(array, pointerSortedItem, i);
+                    pointerSortedItem += 1;
+                }
+            }
+            Swap(array, pointerSortedItem, right);
+
+            return pointerSortedItem;
+        }
+
+        //********************************** MERGE SORT *******************************
+        public static void MergeSort(int[] array)
+        {
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            int leftSize = array.Length / 2;
+            int rightSize = array.Length - leftSize;
+            int[] left = new int[leftSize];
+            int[] right = new int[rightSize];
+
+            Array.Copy(array, 0, left, 0, leftSize);
+            Array.Copy(array, leftSize, right, 0, rightSize);
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(array, left, right);
+
+
+        }
+
+        private static void Merge(int[] array, int[] left, int[] right)
+        {
+            int leftPointer = 0;
+            int rightPointer = 0;
+            int lengthTheArr = right.Length + left.Length;
+
+            for(int i = 0; i < lengthTheArr - 1; i ++)
+            {
+                if (left.Length == leftPointer)
+                {
+                    array[i] = right[rightPointer++];
+                }else if(right.Length == rightPointer)
+                {
+                    array[i] = left[leftPointer++];
+                }else if (left[leftPointer] >= right[rightPointer])
+                {
+                    array[i] = right[rightPointer++];
+                }
+                else if (left[leftPointer] < right[rightPointer])
+                {
+                    array[i] = left[leftPointer++];
+                }
+                
+            }
+
+            if(leftPointer == left.Length)
+            {
+                array[array.Length - 1] = right[rightPointer];
+            }
+
+            if(rightPointer == right.Length)
+            {
+                array[array.Length - 1] = left[leftPointer];
+            }
+        }
+
+    }
+}
