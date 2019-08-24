2b72de2e83522348f50f6708645d45faf040c877
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-08-24 21:13:15 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.08/Book/Service/IBookListService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAT.A._2019.Greben._08.Abstract
+{
+    public interface IBookListService
+    {
+        void AddBook();
+        void RemoveBook();
+
+        List<Book> GetBooks();
+
+        Book FindBookByTag(int id);
+        Book FindBookByTag(string name, string author);
+
+        Book FindBookByTag( string name, string author,int yearPublish);
+
+        void SortByTag();
+        void ShowList();
+
+
+    }
+}
