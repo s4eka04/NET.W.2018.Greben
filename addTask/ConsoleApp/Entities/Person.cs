8212d6894b74f187b49143c37940a97e7bc20f8e
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-08-24 21:28:42 +0300

    Add files via upload

---
+++ addTask/ConsoleApp/Entities/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIleCabinet.Entites
+{
+    public class Person
+    {
+      //  private static int Id { get; set; }
+
+        public int IdPerson { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(int id, string firstName, string lastName, DateTime dateTime)
+        {
+            IdPerson = id;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateTime;
+
+        }
+        public Person(int count)
+        {
+            IdPerson = ++count;
+        }
+        public override string ToString()
+        {
+   
+            return "Person №: " + IdPerson + "\nFirst Name: " +
+                    FirstName + "\nLast Name: " + LastName + "\nDate of Birth: " + DateOfBirth.Day + '/' +
+                    DateOfBirth.Month + '/' + DateOfBirth.Year + '\n';
+        }
+    }
+}
