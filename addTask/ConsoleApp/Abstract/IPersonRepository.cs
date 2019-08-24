c9b6390d84cdd5acd9205b7f37da9246d10c13ab
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-08-24 21:24:27 +0300

    Add files via upload

---
+++ addTask/ConsoleApp/Abstract/IPersonRepository.cs
@@ -0,0 +1,30 @@
+﻿using FIleCabinet.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIleCabinet.Abstract
+{
+    public interface IPersonRepository
+    {
+        void Create();
+        List<Person> List();
+        int State();
+
+        void Edit(Person person);
+
+        Person Find( string firstName);
+
+        void ExportXml(string path);
+        void ExportCSV(string path);
+
+        List<Person> ImportXml(string pathToXmlFile);
+        List<Person> ImportCSV(string pathToXmlFile);
+        void RemoveById(int id);
+
+        void AddAllPersonToMemory(List<Person> persons);
+        void Purge();
+    }
+}
