cc8c497238aac4b0592be59eeee3ac6d88a4e508
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-08-24 21:06:43 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.08/Abstract/IRepositoryAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Abstract
+{
+    public interface IRepositoryAccount
+    {
+        Account CreateAccount();
+        void CloseAccount(int id);
+        void Show();
+        Account GetAccount();
+        void WithDrawMoney(Account account);
+        void PutMoney(Account account);
+
+    }
+}
