6abc9a241d44797f32858a8dc3709fd95b8130dd
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-08-31 14:24:52 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.10/Test for 1-2 tasks/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NAT.A._2019.Greben._08;
+
+namespace TestRepresentation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReperrepresentationTest()
+        {
+            Book book = new Book(1, "Pushkin", "Blizzard", "Boldino", 1830, 250, 250.1m);
+
+            Assert.AreEqual(book.ToString("I"), "1");
+            Assert.AreEqual(book.ToString("I", "A", "N"), "1,Pushkin,Blizzard");
+            Assert.AreEqual(book.ToString("I", "N", "A"), "1,Blizzard,Pushkin");
+        }
+    }
+}
