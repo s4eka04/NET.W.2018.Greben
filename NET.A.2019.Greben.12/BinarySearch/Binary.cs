24463543c3c170c01544aa0d8d845f95a930d7fd
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-09-03 22:23:31 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.12/BinarySearch/Binary.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class Binary<T>
+    {
+       /// <summary>
+       /// Array property
+       /// </summary>
+        public T[] ArrayT { get; private set; }
+
+        /// <summary>
+        /// Delegate
+        /// </summary>
+        Func<T, T, bool> func = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tempArray"></param>
+        public Binary(T[] tempArray)
+        {
+            
+            ArrayT = tempArray;
+        }
+
+        /// <summary>
+        /// Sort Method
+        /// </summary>
+        /// <param name="sortDelegate"></param>
+        public void Sort(Func<T, T, bool> sortDelegate)
+        {
+            if(sortDelegate == null)
+            {
+                throw new Exception();
+            }
+            func = sortDelegate;
+            for (int i = 0; i < ArrayT.Length; i++)
+            {
+                for (int j = 0; j < ArrayT.Length - 1 - i; j++)
+                {
+                    if (func(ArrayT[j], ArrayT[j+1]))
+                    {
+                        Swap(ref ArrayT[j], ref ArrayT[j + 1]);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Binary Search goal
+        /// </summary>
+        /// <param name="goal"></param>
+        /// <param name="equally"></param>
+        /// <returns>Index goal</returns>
+        public int SearchIndex(T goal, Func<T, T, bool> equally)
+        {
+            if(func == null)
+            {
+                throw new Exception();
+            }
+            int indexBegin = 0;
+            int check = 0;
+            int indexEnd = ArrayT.Length;
+            int middle = 0;
+            while (!equally(goal,ArrayT[middle]))
+            {
+                if(check >= Math.Ceiling(Math.Log(ArrayT.Length, 2)))
+                {
+                    throw new Exception("Value not found" + check);
+                }
+                middle = (indexBegin + indexEnd) / 2;
+                if (func(goal, ArrayT[middle]))
+                {
+                    indexBegin = middle;
+                    check++;
+                }
+                else
+                {
+                    indexEnd = middle;
+                    check++;
+                }
+            }
+
+            return middle;
+        }
+
+        /// <summary>
+        /// Swap elements
+        /// </summary>
+        /// <param name="t1"></param>
+        /// <param name="t2"></param>
+        private void Swap(ref T t1, ref T t2)
+        {
+            T temp = t1;
+            t1 = t2;
+            t2 = temp;
+        }
+
+        /// <summary>
+        /// Show Array
+        /// </summary>
+        public void Show()
+        {
+            foreach (var item in ArrayT)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ NET.A.2019.Greben.12/BinarySearch/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinarySearch;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BinarySearchTest()
+        {
+            int[] arrayInt = new int[] { 5, 6, 7, 3, 2, 3, 5, 1 };
+            string[] arrayString = new string[] { "Egor", "grebne", "vlad", "greben", "alex", "pety" };
+
+            Binary<int> binaryInt = new Binary<int>(arrayInt);
+            Binary<string> binaryString = new Binary<string>(arrayString);
+
+            binaryInt.Sort((i, j) => i >= j); // 1 2 3 3 5 5 6 7
+            Assert.AreEqual(7,binaryInt.SearchIndex(7, (i, j) => i == j));  // return index goal
+
+            binaryInt.Sort((i, j) => i <= j); // 7 6 5 5 3 3 2 1
+            Assert.AreEqual(0, binaryInt.SearchIndex(7, (i, j) => i == j));  // --//--
+
+            binaryString.Sort((i, j) => i.ToUpper()[0] > j.ToUpper()[0]); // alex Egor grebne greben pety vlad
+            Assert.AreEqual(2, binaryString.SearchIndex("grebne", (i, j) => i == j));
+        }
+    }
+}
