2bef3eb5779906b516f20f42c6101395a15465c3
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-09-03 22:21:31 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.12/Time/Trial classes/DighitalWatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer.Trial_classes
+{
+    public class DighitalWatch
+    {
+
+        
+        /// <summary>
+        /// Construcor and subscribe
+        /// </summary>
+        /// <param name="timer"></param>
+        public DighitalWatch(Timer timer)
+        {
+            timer.TimerSignal += DighitalMessage;
+        }
+
+        /// <summary>
+        /// Execution method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DighitalMessage(object sender, TimeEventArgs e)
+        {
+            Console.WriteLine("Dighital watch");
+            Console.WriteLine("Time = " + e.TimeInSec + "c, begin = " + e.TimeBegin + ", end = " + e.TimeEnd);
+        }
+
+        /// <summary>
+        /// Unsubscribe
+        /// </summary>
+        /// <param name="timer"></param>
+        public void Unregister(Timer timer)
+        {
+            timer.TimerSignal -= DighitalMessage;
+        }
+    }
+}
---
+++ NET.A.2019.Greben.12/Time/Trial classes/WristWatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer.Trial_classes
+{
+    public class WristWatch
+    {
+        /// <summary>
+        /// Construcor and subscribe
+        /// </summary>
+        /// <param name="timer"></param>
+        public WristWatch(Timer timer)
+        {
+            timer.TimerSignal += WristMessage;
+        }
+
+        /// <summary>
+        /// Execution method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void WristMessage(object sender, TimeEventArgs e)
+        {
+            Console.WriteLine("Wrist Watch");
+            Console.WriteLine("Time = " + e.TimeInSec + "c, begin = " + e.TimeBegin + ", end = " + e.TimeEnd);
+        }
+
+        /// <summary>
+        /// Unsubscribe
+        /// </summary>
+        /// <param name="timer"></param>
+        public void Unregister(Timer timer)
+        {
+            timer.TimerSignal -= WristMessage;
+        }
+    }
+}
