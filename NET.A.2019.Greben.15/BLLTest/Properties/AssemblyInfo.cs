0a116ab4875a32dba639e451f574b589c1d8533d
s4eka04 <52720771+s4eka04@users.noreply.github.com>
2019-09-08 22:55:16 +0300

    Add files via upload

---
+++ NET.A.2019.Greben.15/BLLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BllTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BllTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44abb0f4-f0fd-4982-aaa7-8d32357a2074")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NET.A.2019.Greben.15/BLLTest/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BLL.Interface.Entities;
+using BLL.Interface.Interfaces;
+using BLL.ServiceImplementation;
+using DAL.Interface.DTO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+
+namespace BllTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        Mock<IAccountService> mock = new Mock<IAccountService>();
+
+
+
+        [TestMethod]
+        public void CreateAccountsAndDepositMoneyTest()
+        {
+            List<AccountDTO> accounts = new List<AccountDTO>();
+            accounts.Add(new AccountDTO()
+            {
+                Id = 1,
+                FirstName = "Egor",
+                LastName = "Greben",
+                AccountType = AccountType.Base,
+                MoneyDTO = new MoneyDTO() { Id = 1, Sum = 0, Discount = 0 }
+            });
+
+            accounts.Add(new AccountDTO()
+            {
+                Id = 2,
+                FirstName = "Vlad",
+                LastName = "Greben",
+                AccountType = AccountType.Base,
+                MoneyDTO = new MoneyDTO() { Id = 2, Sum = 0, Discount = 0 }
+            });
+
+
+
+
+
+            mock.Setup(a => a.GetAccount(It.IsAny<int>())).Returns<int>( a => accounts[a - 1]);
+            mock.Setup(a => a.DepositAccount(It.IsAny<int>(), It.IsAny<decimal>())).
+                    Callback<int, decimal>((id, sum) =>
+                    {
+                        accounts[id - 1].MoneyDTO.Sum += sum;
+                        accounts[id - 1].MoneyDTO.Discount = (accounts[id - 1].MoneyDTO.Sum / 50m);
+                    });
+            
+            IAccountService accountService = mock.Object;
+
+            Assert.AreEqual(accountService.GetAccount(1).FirstName, "Egor");
+
+            accountService.DepositAccount(1, 100m);
+            Assert.AreEqual(accountService.GetAccount(1).MoneyDTO.Sum, 100m );
+            Assert.AreEqual(accountService.GetAccount(1).MoneyDTO.Discount, 2m);
+
+            accountService.DepositAccount(1, 100m);
+            Assert.AreEqual(accountService.GetAccount(1).MoneyDTO.Sum, 200m);
+            Assert.AreEqual(accountService.GetAccount(1).MoneyDTO.Discount, 4m);
+
+
+        }
+
+    }
+}
